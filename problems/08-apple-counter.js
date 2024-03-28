187def20bef4bb211916727200dd5f41ba68ea02
Omega Dziko F. <megaoy@yahoo.com>
2024-03-27 20:33:51 -0400

    8th prb slvd

--- problems/08-apple-counter.js
+++ problems/08-apple-counter.js
@@ -1,7 +1,7 @@
 /***********************************************************************
-Write a function `appleCounter(appleObj)` that takes in an object containing a 
+Write a function `appleCounter(appleObj)` that takes in an object containing a
 number of keys that have the word 'apple' contained within them. The `appleCounter`
-function will be in charge of returning the number of keys that contain the word 
+function will be in charge of returning the number of keys that contain the word
 "apple".
 
 **Hint**: you may want to take all the keys and lower case them for easier
@@ -29,7 +29,30 @@ appleCounter(obj3); // => 3
 ***********************************************************************/
 
 function appleCounter(appleObj) {
-  // Your code here 
+
+  //create array of keys and lowercase them
+  let lowerKeys = Object.keys(appleObj).map(key => key.toLowerCase());
+
+  //create a counter starting at zero
+  let count = 0;
+
+  //loop through keys array
+ for (let i = 0; i < lowerKeys.length; i++) {
+
+  //store val in variable
+  let key = lowerKeys[i];
+  
+  //ask if array contains the word apple
+  if(key.includes("apple")) {
+
+  //if yes, increment counter
+  count++;
+
+  }
+
+}
+  //return count
+  return count;
 }
 
 /**************DO NOT MODIFY ANYTHING UNDER THIS  LINE*****************/
